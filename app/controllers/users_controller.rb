1b817f5f2b985676f0bea83840b8a8dacc94f92f
Mahmoud Bakr <mbakr6821@gmail.com>
2020-09-14 12:36:42 +0200

    add the edit and update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,15 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+    end
+
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
