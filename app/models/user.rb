e71a2870beca4b33d5563c07dd38c96e1d51af49
Mahmoud Bakr <mbakr6821@gmail.com>
2020-09-14 11:34:12 +0200

    create method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 6, maximum: 20}
+    validates :username, presence: true, uniqueness: { case_sensitive: false }
     validates :email, presence: true, uniqueness: { case_sensitive: false }
-    validates :password, presence: true, length: { minimum: 8, maximum: 20}
+    validates :password, presence: true, length: { minimum: 4, maximum: 20}
 end
